62b3944dbbc5628359ea4bf363a78b875165e39c
Nikhil <nik.theappideas@gmail.com>
2021-08-10 16:13:15 +0530

    first commit

---
+++ routes/pages.js
@@ -0,0 +1,35 @@
+var express=require('express');
+var router=express.Router();
+
+// GET pages model
+var Page = require('../models/page');
+
+//GET 
+router.get('/',function(req,res){
+    Page.findOne({slug:'home'}, function(err, page){
+        if(err){console.log(err);}
+        res.render('index',{
+                title:page.title,
+                content:page.content
+        });
+    });
+});
+
+//GET a page
+router.get('/:slug',function(req,res){
+    var slug = req.params.slug;
+    Page.findOne({slug:slug}, function(err, page){
+        if(err){console.log(err);}
+        if(!page){
+            res.redirect('/');
+        }else{
+            res.render('index',{
+                title:page.title,
+                content:page.content,
+            });
+        }
+    });
+});
+
+//exports
+module.exports=router;
