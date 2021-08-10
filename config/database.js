43754419031fbf0c62f166f0ce4dda57c88977be
Nikhil <nik.theappideas@gmail.com>
2021-08-10 18:03:59 +0530

    msg

--- config/database.js
+++ config/database.js
@@ -1,1 +1,2 @@
-module.exports=('mongodb://localhost:27017/shopkart');
+// module.exports=('mongodb://localhost:27017/shopkart');
+module.exports=('mongodb+srv://nikhiltai:nikhil2025@shopkart.1yahp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
