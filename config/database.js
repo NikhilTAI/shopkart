b736400e8e7922ad0fc6ecc0e2f3f56d062c1e91
NikhilTAI <87468380+NikhilTAI@users.noreply.github.com>
2021-08-12 12:34:05 +0530

    Delete database.js

--- config/database.js
+++
@@ -1,2 +0,0 @@
-// module.exports=('mongodb://localhost:27017/shopkart');
-module.exports=('mongodb+srv://nikhiltai:nikhil2025@shopkart.1yahp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
